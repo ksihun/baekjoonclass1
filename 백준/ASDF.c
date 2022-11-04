3116c02d53f9ac979d8d0126019ffc778d3c814d
시헌 권 <ksh122445@gmail.com>
2022-11-05 01:49:20 +0900

    check

---
+++ 백준/ASDF.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<limits.h>
+#include<string.h>
+#include<stdlib.h>
+#pragma warning (disable:4996)
+
+int main() {
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A * B);
+
+
+}
